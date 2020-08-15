f0b751e4da3b4e777bf4eeb1ab932a51c3dedbdd
David Monroe <hatguy@yandex.ru>
2020-08-15 16:33:21 +0300

    some more tweaking

--- lsp_model/__init__.py
+++ lsp_model/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.0.1"
 from transformers_dev.tokenization_gpt2 import GPT2Tokenizer
-from transformers_dev.file_utils import PYTORCH_PRETRAINED_BERT_CACHE, cached_path
-from transformers_dev.modeling_gpt2 import GPT2Config, GPT2Model, GPT2Config
+from file_utils.file_utils import PYTORCH_PRETRAINED_BERT_CACHE, cached_path
+from modeling_gpt2 import GPT2Config, GPT2Model, GPT2Config
 from transformers_dev.tokenization_gpt2 import GPT2Tokenizer
 from .tokenization_ruberta import RubertaTokenizer
 
